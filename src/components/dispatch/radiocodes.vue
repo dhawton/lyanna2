029a3c25e59434c10c72a2cc1d6c6545f9e610b2
Daniel Hawton <daniel@hawton.org>
2019-04-21 10:18:10 -0800

    radio codes update

--- src/components/dispatch/radiocodes.vue
+++ src/components/dispatch/radiocodes.vue
@@ -1,5 +1,8 @@
 <template>
-  <iframe src="https://bit.ly/2RCIs0C" style="width: 100%; height:900px;"></iframe>
+  <iframe
+    src="https://docs.google.com/document/d/10hQ2x1zqEJEbYshyy5xX1n4quwWNsEzTzMD0fFj-Yr0/edit?usp=sharing"
+    style="width: 100%; height:900px;"
+  ></iframe>
 </template>
 
 <script>
